e29d67e0866ffc2989383dfd16d0f6111a0b686d
MijaVujnovic <mija.vujnovic2@gmail.com>
2023-10-25 19:41:42 +0200

    drugi i treci wip

---
+++ lab2/zad2.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define max_length (50)
+#define NO_PERSON_FOUND (NULL)
+
+typedef struct _Person* Position;
+
+typedef struct _Person {
+	char name[max_length];
+	char lastname[max_length];
+	int birthYear = 0;
+
+}
+
+
+
+int menu(Position head) {
+	char choice = '\0';
+	Position person = NULL;
+	while (1) {
+	
+
+	}
+
+	                                                                                                              
+}
+
+int main() {
+
+	Person head = { .next = NULL, .name = {0}, .lastname = {0}, .birthYear = {0}
+
+}
